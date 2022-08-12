57a743e0b62cecdbc2da2c6462ab1a0236de82e6
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2022-08-12 16:51:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your code here!
+
+//=======practice===//
+// document.body.append(element);
+// const element1 = document.createElement("div"); document.body.append(element1);
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//   const li = document.createElement("li");
+//   li.textContent = (i + 1).toString();
+//   ul.append(li);
+// }
+
+// element1.append(ul);
+// const element = document.getElementById("main"); element.style.height = "300px"; element.style.backgroundColor = "yellow";
+// element.innerHTML = "<div> </div>";
+
+// element.innerHTML = "<div><p> Here is some inner HTML stuff that <strong> i have added </strong> </p> </div>";
+// '<div><p> Here is some inner HTML stuff that <strong> i have added </strong> </p> </div>'
+// element.textContent = "You have changed what is on the screen";
+// 'You have changed what is on the screen'
+// element.style.fontSize = "24px";
+// element.style.marginLeft = "30px";
+// element.style.lineHeight = 2;
+// element.className = "pet-listing dog";
+// 'pet-listing dog'
+
+// someElement.removeChild(someChildElement);
+// remove child - removeChild()
+
+// //remove element 
+// element.remove();
+
+
+
+// start of assignment
+main.remove();
+const newHeader = document.createElement("h1"); 
+newHeader.id = "victory";
+newHeader.innerHTML = "Jackline is the champion";
+document.body.append(newHeader);
+
+
+
